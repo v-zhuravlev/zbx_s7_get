280c9212b543bc9abc0b78dec424b8ea38a35d23
v-zhuravlev <zhuravlev.vitaly@gmail.com>
2019-12-26 15:52:22 +0300

    Add bit_index for bool

--- s7_get.py
+++ s7_get.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import argparse
 import ipaddress
 import snap7
@@ -7,7 +8,7 @@ import snap7
 def create_parser():
 
     helptext = """
-    This implements S7 for Zabbix. Add this script to external script dir
+    This implements S7 for Zabbix. Add this script to external script dir 
     on Zabbix proxy or Zabbix server.
     snap7 must be installed as well as python-snap7 wrapper.
 
@@ -21,8 +22,8 @@ def create_parser():
     parser.add_argument("rack", type=int)
     parser.add_argument("slot", type=int)
     parser.add_argument("DB", type=int),
-    parser.add_argument("offset", type=int, default=0)
-    parser.add_argument("datatype", metavar="datatype", type=str,
+    parser.add_argument("offset", type=str, help="For bool, set as '6.1' to get bit 1 of byte 6")
+    parser.add_argument("datatype", metavar="datatype", type=str, 
                         choices=['int', 'float', 'bool', 'string'])
     # For string, you should explicitly set number of bytes to read
     parser.add_argument("bytes_to_read", type=int, nargs="?")
@@ -33,20 +34,22 @@ if __name__ == "__main__":
 
     args = create_parser()
     plc = snap7.client.Client()
+    # plc.connect("PLC IP address",rack,slot)
 
     if args.datatype == 'string':
         raise NotImplementedError("string datatype not implemented yet.")
-
-    if args.datatype == 'string' and not args.bytes_to_read:
-        raise argparse.ArgumentError(
-            'Please provide number of bytes to read when using string.')
+        if not args.bytes_to_read:
+            raise argparse.ArgumentError('Please provide number of bytes to read when using string.')
 
     if args.datatype == 'int':
         bytes_to_read = 2
+        offset = int(args.offset)
     elif args.datatype == 'float':
         bytes_to_read = 4
+        offset = int(args.offset)
     elif args.datatype == 'bool':
         bytes_to_read = 1
+        offset, bit_index = map(int, args.offset.split(".", 2))
 
     if args.bytes_to_read:
         bytes_to_read = args.bytes_to_read
@@ -56,7 +59,7 @@ if __name__ == "__main__":
                 args.slot)
 
     bytes_response = plc.db_read(args.DB,
-                                 args.offset,
+                                 offset,
                                  bytes_to_read)
 
     plc.disconnect()
@@ -66,9 +69,9 @@ if __name__ == "__main__":
     elif args.datatype == 'float':
         response = snap7.util.get_real(bytes_response, 0)
     elif args.datatype == 'bool':
-        response = snap7.util.get_bool(bytes_response, 0, 0)
+        response = snap7.util.get_bool(bytes_response, 0, bit_index)
     elif args.datatype == 'string':
         pass
     #   response = snap7.util.get_string(bytes_response, 0, bytes_to_read)
-
+    
     print(response)
